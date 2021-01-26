0ef7ac1e654fd176a8dce81f735f3723e4711d34
Alejandro Lopez <alejandro.lopez@lottoland.com>
2021-01-26 11:22:11 +0100

    Refactored the beans II

--- src/main/java/guru/springframework/services/GreetingServiceFactory.java
+++ src/main/java/guru/springframework/services/GreetingServiceFactory.java
@@ -2,7 +2,7 @@ package guru.springframework.services;
 
 public class GreetingServiceFactory {
 
-    private GreetingRepository greetingRepository;
+    private final GreetingRepository greetingRepository;
 
     public GreetingServiceFactory(GreetingRepository greetingRepository) {
         this.greetingRepository = greetingRepository;
@@ -10,8 +10,6 @@ public class GreetingServiceFactory {
 
     public GreetingService createGreetingService(String lang) {
         switch (lang) {
-            case "en" :
-                return new PrimaryGreetingService(greetingRepository);
             case "de" :
                 return new PrimaryGermanGreetingService(greetingRepository);
             case "es" :
